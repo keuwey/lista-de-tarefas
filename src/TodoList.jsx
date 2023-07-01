acef2210e49c37cb847c9556f4acc9f7e037e900
keuwey <keuwey@protonmail.com>
2023-06-30 21:07:15 -0300

    Adding the 'done' function

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
-import "./TodoList.css";
+import React, { useState } from "react"
+import "./TodoList.css"
+import Icone from "./assets/lista.png"
 
 function TodoList() {
-  const [lista, setLista] = useState([]);
-  const [novoItem, setNovoItem] = useState("");
+  const [lista, setLista] = useState([])
+  const [novoItem, setNovoItem] = useState("")
+
+  function adicionaItem(form) {
+    form.preventDefault()
+    if (!novoItem) return
+    setLista([...lista, { text: novoItem, isComplited: false }])
+    setNovoItem("")
+    document.getElementById(".input-entrada").focus()
+  }
+
+  function clicou(index) {
+    const listaAux = [...lista]
+    listaAux[index].isComplited = !listaAux[index].isComplited
+    setLista(listaAux)
+  }
+
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <form>
+      <form onSubmit={adicionaItem}>
         <input
+          id="input-entrada"
           type="text"
           value={novoItem}
           onChange={(e) => {
-            setNovoItem(e.target.value);
+            setNovoItem(e.target.value)
           }}
           placeholder="Adicione uma tarefa"
         />
@@ -21,18 +38,29 @@ function TodoList() {
         </button>
       </form>
       <div className="listaTarefas">
-        <div className="item">
-          <span>Tarefa de Exemplo</span>
-          <button className="del">Deletar</button>
-        </div>
-        <div className="item completo">
-          <span>Tarefa de Exemplo</span>
-          <button className="del">Deletar</button>
+        <div style={{ textAlign: "center" }}>
+          {lista.length < 1 ? (
+            <img src={Icone} className="listaLogo" />
+          ) : (
+            lista.map((item, index) => (
+              <div
+                key={index}
+                className={item.isComplited ? "item completo" : "item"}>
+                <span
+                  onClick={() => {
+                    clicou(index)
+                  }}>
+                  {item.text}
+                </span>
+                <button className="del">Deletar</button>
+              </div>
+            ))
+          )}
         </div>
         <button className="deleteAll">Deletar Todas</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default TodoList;
+export default TodoList
